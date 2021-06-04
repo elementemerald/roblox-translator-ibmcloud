2d55e978725c825a5a28ff8a831ea6043193352f
elementemerald <22267679+elementemerald@users.noreply.github.com>
2021-06-03 21:07:22 -0600

    Fix for HTTP Requests

--- main.lua
+++ main.lua
@@ -5,25 +5,35 @@ local StarterGui = game:GetService("StarterGui");
 local serviceUrl = "";
 local apiKey = "";
 
-local function getBase64(data)
+local function getSpecificFunc(type, data)
     local exec = identifyexecutor();
-    if exec == "ScriptWare" then
-        return crypt.base64encode(data);
-    elseif exec == "Synapse X" then
-        return syn.crypt.base64.encode(data);
-    else
-        error("Your platform is not supported");
+    if type == "base64" then
+        if exec == "ScriptWare" then
+            return crypt.base64encode(data);
+        elseif exec == "Synapse X" then
+            return syn.crypt.base64.encode(data);
+        else
+            error("Your platform is not supported");
+        end;
+    elseif type == "request" then
+        if exec == "ScriptWare" then
+            return http.request(data);
+        elseif exec == "Synapse X" then
+            return syn.request(data);
+        else
+            error("Your platform is not supported");
+        end;
     end;
 end;
 
-local apiKeySerialized = string.format("Basic %s", getBase64("apikey:" ..apiKey));
+local apiKeySerialized = string.format("Basic %s", getSpecificFunc("base64", "apikey:" ..apiKey));
 
 local translateTo = "en";
 
 local translateMsg = function(v, msg)
     -- Detect language using AI
     
-    local req = syn.request({
+    local req = getSpecificFunc("request", {
         Url = string.format("%s/v3/identify?version=2018-05-01", serviceUrl),
         Method = "POST",
         Headers = {
@@ -43,7 +53,7 @@ local translateMsg = function(v, msg)
         ["model_id"] = string.format("%s-%s", translateFrom, translateTo)
     };
     
-    local req2 = syn.request({
+    local req2 = getSpecificFunc("request", {
         Url = string.format("%s/v3/translate?version=2018-05-01", serviceUrl),
         Method = "POST",
         Headers = {
